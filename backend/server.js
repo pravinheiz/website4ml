df796a98f13ef46ace764ab8e10969c98df0ff8c
Heisnam Pravin Singh <123856618+pravinheiz@users.noreply.github.com>
2025-07-02 20:39:41 +0530

    Create server.js

---
+++ backend/server.js
@@ -0,0 +1,14 @@
+{
+  "name": "auth-backend",
+  "version": "1.0.0",
+  "scripts": {
+    "start": "node server.js"
+  },
+  "dependencies": {
+    "express": "^4.18.2",
+    "bcryptjs": "^2.4.3",
+    "crypto": "^1.0.1",
+    "pg": "^8.11.3",
+    "nodemailer": "^6.9.8"
+  }
+}
